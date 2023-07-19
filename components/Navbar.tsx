9456765e54590d8f94c150d049a54c3956b70298
Airat Galiev <airat.galiev@gmail.com>
2023-07-19 07:16:32 +0600

    add google login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavLinks } from "@/constant";
 import { AuthProviders } from "@/components/AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import { ProfileMenu } from "@/components/ProfileMenu";
 
-export const Navbar = () => {
-  const session = {};
+export const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -20,9 +22,9 @@ export const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            <ProfileMenu session={session} />
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
